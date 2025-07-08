8f5da0fd01b5b41795a84ba38977c048e2dfdedd
cruizba <carlos.ruizbal@gmail.com>
2025-07-08 12:05:37 +0200

    Do not configure LAN_PRIVATE_IP in openvidu if LAN_MODE is not true.

--- community/scripts/entrypoint_openvidu.sh
+++ community/scripts/entrypoint_openvidu.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ "$LAN_PRIVATE_IP" != "" ]; then
+if [ "$LAN_PRIVATE_IP" != "" ] && [ "$LAN_MODE" = 'true' ]; then
+    echo "Using as NODE_IP: $LAN_PRIVATE_IP"
     export NODE_IP="$LAN_PRIVATE_IP"
 fi
 

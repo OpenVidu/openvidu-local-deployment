8e644bddb89d6588f27dacb7355fee1ac8ef7bb9
cruizba <carlos.ruizbal@gmail.com>
2025-09-17 20:28:29 +0200

    Add MEET_BASE_URL

--- community/scripts/entrypoint_openvidu_meet.sh
+++ community/scripts/entrypoint_openvidu_meet.sh
@@ -2,7 +2,12 @@
 
 . /scripts/utils.sh
 
-URL=$(getDeploymentUrl ws)
-export LIVEKIT_URL="${URL}"
+LIVEKIT_URL=$(getDeploymentUrl ws)
+MEET_BASE_URL=$(getDeploymentUrl http meet)
+export LIVEKIT_URL="${LIVEKIT_URL}"
+export MEET_BASE_URL="${MEET_BASE_URL}"
+
+echo "LIVEKIT_URL is set to ${LIVEKIT_URL}"
+echo "MEET_BASE_URL is set to ${MEET_BASE_URL}"
 
 /usr/local/bin/entrypoint.sh
--- pro/scripts/utils.sh
+++ pro/scripts/utils.sh
@@ -2,17 +2,31 @@
 
 getDeploymentUrl() {
     schema="${1:-http}"
-    URL="$schema://localhost:7880"
-    if [ "${USE_HTTPS}" = 'true' ]; then
-        URL="${schema}s://localhost:7443"
+    service="${2:-default}"
+
+    if [ "$service" = "meet" ]; then
+        URL="$schema://localhost:9080"
+        if [ "${USE_HTTPS}" = 'true' ]; then
+            URL="${schema}s://localhost:9443"
+        fi
+    else
+        URL="$schema://localhost:7880"
+        if [ "${USE_HTTPS}" = 'true' ]; then
+            URL="${schema}s://localhost:7443"
+        fi
     fi
+
     if [ "${LAN_MODE}" = 'true' ]; then
         LAN_DOMAIN=${LAN_DOMAIN:-"openvidu-local.dev"}
         if [ "$LAN_PRIVATE_IP" != 'none' ] && [ "${LAN_DOMAIN}" = 'openvidu-local.dev' ]; then
             # Replace dots with dashes
             LAN_DOMAIN="$(echo "$LAN_PRIVATE_IP" | sed 's/\./-/g').openvidu-local.dev"
         fi
-        URL="${schema}s://${LAN_DOMAIN}:7443"
+        if [ "$service" = "meet" ]; then
+            URL="${schema}s://${LAN_DOMAIN}:9443"
+        else
+            URL="${schema}s://${LAN_DOMAIN}:7443"
+        fi
     fi
     echo "$URL"
 }

7530ce1316ce057d1f57e30cba4a10336fdaba29
Stephan Eggermont <stephan@stack.nl>
2025-10-27 14:16:55 +0100

    typo configure_lan_private_ip_macos.sh

--- pro/configure_lan_private_ip_macos.sh
+++ pro/configure_lan_private_ip_macos.sh
@@ -16,7 +16,7 @@ fi
 # Replace the LAN_PRIVATE_IP in the .env file
 sed -i'' -e "s/LAN_PRIVATE_IP=.*/LAN_PRIVATE_IP=$LAN_PRIVATE_IP/g" .env
 
-# If sillicon mac, enable EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU flag
+# If silicon mac, enable EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU flag
 if [ "$(uname -m)" = "arm64" ]; then
     if ! grep -q "EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU" .env; then
         echo "# Enable this flag to run Docker Desktop on Apple Silicon Macs" >> .env

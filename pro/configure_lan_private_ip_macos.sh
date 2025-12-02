0e1a3cfea0f5c9a221bbe7acd522f023f4672b32
cruizba <carlos.ruizbal@gmail.com>
2025-12-02 13:31:22 +0100

    Add arm64 support

--- pro/configure_lan_private_ip_macos.sh
+++ pro/configure_lan_private_ip_macos.sh
@@ -15,13 +15,3 @@ fi
 
 # Replace the LAN_PRIVATE_IP in the .env file
 sed -i'' -e "s/LAN_PRIVATE_IP=.*/LAN_PRIVATE_IP=$LAN_PRIVATE_IP/g" .env
-
-# If sillicon mac, enable EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU flag
-if [ "$(uname -m)" = "arm64" ]; then
-    if ! grep -q "EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU" .env; then
-        echo "# Enable this flag to run Docker Desktop on Apple Silicon Macs" >> .env
-        echo "EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU=1" >> .env
-    else
-        sed -i'' -e "s/EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU=.*/EXPERIMENTAL_DOCKER_DESKTOP_FORCE_QEMU=1/g" .env
-    fi
-fi
